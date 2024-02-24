17e6fda16a15427e27dac428085aedb11fb8aa87
FerTorron <fertorron21@gmail.com>
2024-02-24 17:33:06 -0300

    Agrego el usuario a la base de datos

--- front/src/util/LoginButton.jsx
+++ front/src/util/LoginButton.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
   const { loginWithRedirect, logout, user, isAuthenticated, isLoading } =
     useAuth0();
 
-  //console.log(user);
+  useEffect(() => {
+    if (isAuthenticated) {
+      enviarSolicitudPost();
+    }
+  }, [isAuthenticated]);
+
+  const enviarSolicitudPost = async () => {
+    if (!user) {
+      return;
+    }
+
+    const url = 'https://no-country-cwv9.onrender.com/api/users/create';
+    const parametros = {
+      nickname: user.nickname,
+      email: user.email,
+      picture: user.picture,
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(parametros),
+      });
+
+      if (response.ok) {
+        console.log('Solicitud POST enviada con éxito');
+      } else if (response.status === 409) {
+        console.error('Usuario ya Registrado');
+      }
+    } catch (error) {
+      console.error('Error al enviar la solicitud POST:', error);
+    }
+  };
+
+  // console.log(user);
 
   if (isAuthenticated) {
     return (
