6ce4b3065c621cf2df2b224dfb9d1d392c776d9f
FerTorron <fertorron21@gmail.com>
2024-02-28 16:31:10 -0300

    Agrego la funcion de eliminar un prod de lcarrito y vaciarlo + documentacion

--- back/src/routes/carts.js
+++ back/src/routes/carts.js
@@ -7,5 +7,7 @@ router.get("/:id", CartController.getCartById);
 router.get("/search/:email", CartController.getCartByEmail);
 router.post("/create", CartController.createCartEmpty);
 router.post("/add/:cid/product/:pid", CartController.addProdInCart);
+router.delete("/delete/:cid/product/:pid", CartController.deleteProdInCart);
+router.delete("/delete/:cid", CartController.emptyCart);
 
 export default router;
